be34f9947e016af97f39a5063f28635619b85ab1
khw <khw0726@kaist.ac.kr>
2023-08-25 01:41:09 +0900

    made some API structure changes

--- Pipeline/api.py
+++ Pipeline/api.py
@@ -105,7 +105,7 @@ df = pd.read_csv("../Datasets/owid-energy-data.csv")
 """
 
 @app.post("/potential_data_point_sets")
-def potential_data_point_sets(body: UserClaimBody, verbose:bool=False, test=True) -> list[DataPointSet]:
+def potential_data_point_sets(body: UserClaimBody, verbose:bool=False, test=False) -> list[DataPointSet]:
     user_claim = body.userClaim.lower()
 
     if test: # for testing purposes
@@ -144,52 +144,40 @@ def get_viz_spec(body: GetVizSpecBody): # needs update
     }
 
     return skeleton_spec
-
-@app.post("/get_data_old")
-def get_data(body: GetVizDataBody) -> str:
-    countries = body.countries
-    date_start = body.date_start
-    date_end = body.date_end
-    dates = [(i) for i in range(date_start, date_end + 1)]
-    categories = ['country', 'year'] + body.categories # add country and year to the list of categories
-
-    # select rows with dates
-    dataframe = df[df['year'].isin(dates)]
-    # select rows with countries
-    dataframe = dataframe[dataframe['country'].isin(countries)]
-    # select columns with categories
-    dataframe = dataframe[categories]
-    dataframe.rename(columns={'year': 'date'}, inplace=True)
-
-
-    res_json = dataframe.to_json(orient='records')
-    return res_json
-
 @app.post("/get_data")
-def get_data_new(body: GetVizDataBodyNew) -> str:
+def get_data_new(body: GetVizDataBodyNew) -> list[dict]:
 
-    otherFieldNames = list(map(lambda x: x, body.otherFields))
-
-    date_start = int(body.date.date_start.value)
-    date_end = int(body.date.date_end.value)
+    otherFieldNames = list(map(lambda x: x, body.fields))
+    ## remove date from otherFieldsNames
+    otherFieldNames.remove('date')
+    if 'date' in body.fields:
+        date_start = int(body.fields['date'].date_start.value)
+        date_end = int(body.fields['date'].date_end.value)
+        dates = [(i) for i in range(date_start, date_end + 1)]
+    else:
+        dates = None
     values = list(map(lambda x: x.value, body.values))
-    dates = [(i) for i in range(date_start, date_end + 1)]
     categories = otherFieldNames + ['year'] + values # add country and year to the list of categories
 
     # select rows with dates
-    dataframe = df[df['year'].isin(dates)]
-    # select rows with countries
+    dataframe = df[df['year'].isin(dates)] if dates is not None else df
     for of in otherFieldNames:
-        otherFieldValue = list(map(lambda x: x.value, body.otherFields[of]))
+        otherFieldValue = list(map(lambda x: x.value, body.fields[of]))
         dataframe = dataframe[dataframe[of].isin(otherFieldValue)]
-    # dataframe = dataframe[dataframe['country'].isin(countries)]
-    # select columns with categories
     dataframe = dataframe[categories]
     dataframe.rename(columns={'year': 'date'}, inplace=True)
 
-
-    res_json = dataframe.to_json(orient='records')
-    return res_json
+    res_dict = dataframe.to_dict(orient='records')
+    for r in res_dict:
+        r['fields'] = {}
+        for of in otherFieldNames:
+            r['fields'][of] = r[of]
+            del r[of]
+        if dates:
+            r['fields']['date'] = r['date']
+            del r['date']
+        
+    return res_dict
 
 
 if __name__ == "__main__":
